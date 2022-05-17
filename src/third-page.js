36092484623824401c1e8dc83c4f166279616fc5
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-05-17 20:21:49 +0400

    feat: add left slideshow animation and navigation to the third page

--- src/third-page.js
+++ src/third-page.js
@@ -1,13 +1,18 @@
-import { pageRightSlideAnimation } from './index.js';
+import { pageRightSlideAnimation, pageLeftSlideAnimation } from './index.js';
 const { log: l } = console;
 const thirdPageNavBtn = document.querySelectorAll('.third-page-nav');
 const currentPageInx = 2;
 
 thirdPageNavBtn.forEach((el, inx) => {
-    if (inx > 2) {
+    if (inx > currentPageInx) {
         el.addEventListener('click', () => {
             const selectedPageInx = inx;
             pageRightSlideAnimation(el, currentPageInx, selectedPageInx);
+        });
+    } else if(inx < currentPageInx){
+        el.addEventListener('click', () => {
+            const selectedPageInx = inx;
+            pageLeftSlideAnimation(el, currentPageInx, selectedPageInx);
         });
     }
 })
