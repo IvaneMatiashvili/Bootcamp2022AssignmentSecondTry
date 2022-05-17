363f6f7b750c5f5b3dd828aacd4db3e0b7beefc8
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-05-17 20:21:36 +0400

    feat: add left slideshow animation and navigation to the second page

--- src/second-page.js
+++ src/second-page.js
@@ -1,13 +1,18 @@
-import { pageRightSlideAnimation } from './index.js';
+import { pageRightSlideAnimation, pageLeftSlideAnimation } from './index.js';
 const { log: l } = console;
 const secondPageNavBtn = document.querySelectorAll('.second-page-nav');
 const currentPageInx = 1;
 
 secondPageNavBtn.forEach((el, inx) => {
-    if (inx > 1) {
+    if (inx > currentPageInx) {
         el.addEventListener('click', () => {
             const selectedPageInx = inx;
             pageRightSlideAnimation(el, currentPageInx, selectedPageInx);
+        });
+    } else if (inx < currentPageInx) {
+        el.addEventListener('click', () => {
+            const selectedPageInx = inx;
+            pageLeftSlideAnimation(el, currentPageInx, selectedPageInx);
         });
     }
 })
