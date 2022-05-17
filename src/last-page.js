d29f9e900b58b7de9d6a8684ced7d04ca816fb0a
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-05-17 20:22:32 +0400

    feat: add left slideshow animation and navigation to the last page

---
+++ src/last-page.js
@@ -0,0 +1,9 @@
+import { pageLeftSlideAnimation } from './index.js';
+const { log: l } = console;
+const goBackBtn = document.querySelector('.go-back-btn');
+
+const [currentPageInx, selectedPageInx] = [4, 3];
+
+goBackBtn.addEventListener('click', () => {
+    pageLeftSlideAnimation(goBackBtn, currentPageInx, selectedPageInx);
+});
