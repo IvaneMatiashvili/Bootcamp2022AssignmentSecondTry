edb9f72dd70cbb09005e322102235c1dbe1f6501
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-05-17 20:34:51 +0400

    fix: remove btn parameter
    btn was declared but its value was never read

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ const leftSide = document.querySelector('.left-side');
 const rightSide = document.querySelector('.right-side');
 const nextArrowBtn = document.querySelectorAll('.go-next');
 
-export const pageRightSlideAnimation = (btn, currentPageInx, selectedPageInx) => {
+export const pageRightSlideAnimation = (currentPageInx, selectedPageInx) => {
     let rightPageTransition = document.querySelector('.transition');
+    
     currentPageInx = currentPageInx + 1;
     selectedPageInx = selectedPageInx + 1;
     rightPageTransition = pages[currentPageInx];
+
     pages[currentPageInx].style.display = 'none';
     pages[selectedPageInx].classList.remove('left-animation');
     pages[selectedPageInx].classList.remove('right-animation');
+
     if (selectedPageInx === 1) {
         [pages[selectedPageInx].style.display, rightPageTransition.style.display] = ['flex', 'block'];
         [leftSide.style.height, rightSide.style.height] = ['100vh', '100vh'];
@@ -21,22 +24,27 @@ export const pageRightSlideAnimation = (btn, currentPageInx, selectedPageInx) =>
     } else {
         [pages[selectedPageInx].style.display, rightPageTransition.style.display] = ['flex', 'flex'];
     }
+
     rightPageTransition.classList.add('right-is-active');
     rightPageTransition.classList.add('right-animation');
+
     setTimeout(() => {
         rightPageTransition.classList.remove('right-is-active');
         [leftSide.style.height, rightSide.style.height] = ['100%', '100%'];
     }, 5);
 }
 
-export const pageLeftSlideAnimation = (btn, currentPageInx, selectedPageInx) => {
+export const pageLeftSlideAnimation = (currentPageInx, selectedPageInx) => {
     let leftPageTransition = document.querySelector('.transition');
+
     currentPageInx = currentPageInx + 1;
     selectedPageInx = selectedPageInx + 1;
     leftPageTransition = pages[currentPageInx];
+
     pages[currentPageInx].style.display = 'none';
     pages[selectedPageInx].classList.remove('left-animation');
     pages[selectedPageInx].classList.remove('right-animation');
+
     if (selectedPageInx === 4) {
         [pages[selectedPageInx].style.display, leftPageTransition.style.display] = ['flex', 'block'];
         [leftSide.style.height, rightSide.style.height] = ['100vh', '100vh'];
@@ -46,8 +54,10 @@ export const pageLeftSlideAnimation = (btn, currentPageInx, selectedPageInx) => 
     } else {
         [pages[selectedPageInx].style.display, leftPageTransition.style.display] = ['flex', 'flex'];
     }
+
     leftPageTransition.classList.add('left-is-active');
     leftPageTransition.classList.add('left-animation');
+    
     setTimeout(() => {
         leftPageTransition.classList.remove('left-is-active');
         [leftSide.style.height, rightSide.style.height] = ['100%', '100%'];
