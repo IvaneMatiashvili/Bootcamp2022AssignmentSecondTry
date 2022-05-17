2be7d0c2d011f8849e2a8a3e52b39c1583f59c0c
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-05-17 20:22:07 +0400

    feat: add left slideshow animation and navigation to the fourth page

--- src/fourth-page.js
+++ src/fourth-page.js
@@ -1,13 +1,18 @@
-import { pageRightSlideAnimation } from './index.js';
+import { pageRightSlideAnimation, pageLeftSlideAnimation } from './index.js';
 const { log: l } = console;
 const fourthPageNavBtn = document.querySelectorAll('.fourth-page-nav');
 const currentPageInx = 3;
 
 fourthPageNavBtn.forEach((el, inx) => {
-    if (inx > 3) {
+    if (inx > currentPageInx) {
         el.addEventListener('click', () => {
             const selectedPageInx = inx;
             pageRightSlideAnimation(el, currentPageInx, selectedPageInx);
+        });
+    } else if (inx < currentPageInx) {
+        el.addEventListener('click', () => {
+            const selectedPageInx = inx;
+            pageLeftSlideAnimation(el, currentPageInx, selectedPageInx);
         });
     }
 })
